d34dcda5ec590460be6776e089e6c01b1628a4ae
t.kajdrowicz <t.kajdrowicz@aotm.gov.pl>
2023-02-20 12:50:33 +0100

    once again resolved conflicts

--- backend_core/config/settings.py
+++ backend_core/config/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.AllowAny'
     ]
 }
 
