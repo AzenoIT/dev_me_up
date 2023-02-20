7cbad45dfbc4badfe36146f9a80eabacc554dfb3
Rafał Płoszański <rafalploszanski@interia.pl>
2023-02-20 13:56:42 +0100

    add schema to urls

--- backend_core/config/urls.py
+++ backend_core/config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('__debug__/', include('debug_toolbar.urls'))
 ]
