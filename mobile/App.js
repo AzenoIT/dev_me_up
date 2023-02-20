f699a818560c9ac9a9db2027d76f17fe5ad28de2
Pawel Konior <pawel.konior@gmail.com>
2023-02-20 14:50:45 +0100

    example of paper material design

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {Provider as PaperProvider, ActivityIndicator, MD2Colors} from 'react-native-paper';
+import {StatusBar} from 'expo-status-bar';
+
+import {theme} from "./theme/theme";
+import MenuBar from "./components/MenuBar/MenuBar";
 
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Say my name</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SafeAreaProvider>
+            <PaperProvider theme={theme}>
+                <View style={styles.container}>
+                    <Text>Say my name</Text>
+                    <Button icon="camera" title={'elo'}>
+                        Press me
+                    </Button>
+
+                    <ActivityIndicator animating={true} color={MD2Colors.red800}/>
+                    <StatusBar style="auto"/>
+                    <MenuBar/>
+                </View>
+            </PaperProvider>
+        </SafeAreaProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
