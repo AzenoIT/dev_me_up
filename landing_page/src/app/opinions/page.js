8d73a1f874dfebe1151090762353dd16ff5276f0
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2023-02-20 18:01:02 +0100

    base opinions

---
+++ landing_page/src/app/opinions/page.js
@@ -0,0 +1,19 @@
+'use client'
+
+import '../../styles/opinions.css'
+
+import {Button} from "@mui/material";
+
+
+function Opinions() {
+    return (
+        <div className='container'>
+            <h2 className='hdl'>Zabawa i nauka w jednym</h2>
+            <p className='text'>Sprawdź swoją wiedzę rywalizując ze znajomymi</p>
+            <Button className='btnAppStore' variant="outlined">Pobierz w App Store</Button>
+            <Button className='btnGooglePlay' variant="outlined">Pobierz z Google Play</Button>
+        </div>
+    );
+}
+
+export default Opinions;
