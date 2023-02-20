42dd0b28780b945fe9d7920ec4e2898dbd95f153
Aleksander Kluska <kluskaolek2002@gmail.com>
2023-02-20 22:09:12 +0100

    uniqe name in technologies model

--- backend_core/technologies/models.py
+++ backend_core/technologies/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Technologies(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
