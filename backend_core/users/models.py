d183558f9733a62d1ae2ebcfaa84b47608090b7a
Bartosz Baran <bartosz.baran192@gmail.com>
2023-02-20 15:17:28 +0100

    logging by email address

--- backend_core/users/models.py
+++ backend_core/users/models.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class CustomUser(AbstractUser):
-    pass
+    email = models.EmailField(_("email address"), unique=True)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__ (self):
+        return self.email
