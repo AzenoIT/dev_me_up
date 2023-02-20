0d5f91f28477679ca63681b1cb7032530c19fa5f
Andrzej Legutko <andrzejlegutko@gmail.com>
2023-02-20 11:22:30 +0100

    fixes .gitignore

--- mobile/App.js
+++ mobile/App.js
@@ -1,14 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Subheader } from 'react-native-material-design';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Subheader text="Normal Subheader"/>
-      <Subheader text="Normal Subheader with color" color="paperRed" />
-      <Subheader text="Normal Subheader" inset />
-      <Subheader text="Normal Subheader with color" color="paperOrange" inset />
       <Text>Say my name</Text>
       <StatusBar style="auto" />
     </View>
