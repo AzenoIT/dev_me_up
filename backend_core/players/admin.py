f3a5401e81f5754e34099f7bd14e9cab638006b4
Martyna Witkowska <mar.witkowska@gmail.com>
2023-02-20 23:09:23 +0100

    add player model, with custom admin and simple serializer

--- backend_core/players/admin.py
+++ backend_core/players/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Player
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    fields = ('pk', 'nick', 'rank', 'is_bot')
+    list_display = ('pk', 'nick', 'rank', 'is_bot')
+    search_fields = ('is_bot',)
+    list_filter = ('is_bot', 'nick')
+    ordering = ('-rank',)
+
+admin.site.register(Player, PlayerAdmin)
--- backend_core/players/models.py
+++ backend_core/players/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    nick = models.CharField(max_length=30)
+    rank = models.PositiveSmallIntegerField()
+    is_bot = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nick
