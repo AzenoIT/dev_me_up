9c85cb90117eac3c639896e5fd1afffdfe595af2
MWasile <maaciej.wasilewski@gmail.com>
2023-02-20 23:45:03 +0100

    serializer, create apiview, urls

--- backend_core/users/views.py
+++ backend_core/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+from . import serializers
+
+class CreateUserAPIView(CreateAPIView):
+    serializer_class = serializers.CustomUserSerializer
+    queryset = get_user_model().objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
