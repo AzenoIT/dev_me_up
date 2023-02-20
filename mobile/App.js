23c5d04bad1f4ff791c35b664a142ee518d9b250
kkulek <106318909+kkulek@users.noreply.github.com>
2023-02-20 13:27:28 +0100

    state for check

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import MyStack from './components/navigation/MyStack'
 
 export default function App() {
   return (
