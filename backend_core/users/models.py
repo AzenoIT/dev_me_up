a7d0b3a19e04a7ca03fea3aa1aa1f76d95d069b6
Bartosz Baran <bartosz.baran192@gmail.com>
2023-02-20 16:01:12 +0100

    fix username unique

--- backend_core/users/models.py
+++ backend_core/users/models.py
@@ -4,7 +4,12 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomUser(AbstractUser):
     email = models.EmailField(_("email address"), unique=True)
-
+    username = models.CharField( _("username"),
+        max_length=100,
+        unique=False,
+        help_text=_(
+            "Required. 100 characters or fewer."
+        ),)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
