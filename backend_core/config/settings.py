66321fa86a326ad0c4e941d2efbefa3476a0b800
BlazejBielski <Blazej Bielski>
2023-02-21 09:25:02 +0100

    change celery broker conf

--- backend_core/config/settings.py
+++ backend_core/config/settings.py
@@ -167,5 +167,5 @@ SPECTACULAR_SETTINGS = {
 
 # Celery configuration
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://0.0.0.0:6379/0'
+CELERY_RESULT_BACKEND = 'redis://0.0.0.0:6379/0'
