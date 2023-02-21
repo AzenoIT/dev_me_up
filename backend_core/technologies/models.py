940525880d7b1cb85aba9324ad07ee168ac9cfa0
Aleksander Kluska <kluskaolek2002@gmail.com>
2023-02-21 11:27:17 +0100

    technologies to players model

--- backend_core/technologies/models.py
+++ backend_core/technologies/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 
-class Technologies(models.Model):
+class Technology(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name_plural = 'Technologies'
+
+
+class TechnologiesToPlayers(models.Model):
+    player_rank = models.FloatField()
+    technology = models.ForeignKey('technologies.Technology', on_delete=models.DO_NOTHING, related_name='%(class)s')
+    player = models.ForeignKey('players.Player', on_delete=models.DO_NOTHING, related_name='%(class)s')
