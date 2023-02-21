5db544b9125d7f3776c4810957ff98a39b31cc9a
t.kajdrowicz <t.kajdrowicz@aotm.gov.pl>
2023-02-21 12:33:46 +0100

    user friend model

--- backend_core/users/models.py
+++ backend_core/users/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -13,7 +14,16 @@ class CustomUser(AbstractUser):
                                 ), )
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["email address"]
 
     def __str__(self):
         return self.email
+
+
+class UserFriend(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='user')
+    friend = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='friend')
+
+    approved = models.BooleanField(default=False)
+    def __str__(self):
+        return f'{self.user} has {self.friend} as a friend'
