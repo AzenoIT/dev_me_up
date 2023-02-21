22ef4acb389974108fd1a95f8f53a9220551fd4b
BlazejBielski <Blazej Bielski>
2023-02-21 15:53:23 +0100

    name space

--- backend_core/technologies/urls.py
+++ backend_core/technologies/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'technologies'
+
 urlpatterns = [
     path('technologies/', views.TechnologyListView.as_view(), name='list_technologies'),
 
