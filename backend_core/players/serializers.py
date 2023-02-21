cb470936bb95c27efb54569a7bd5e64705a1b266
Bartosz Baran <bartosz.baran192@gmail.com>
2023-02-21 23:14:28 +0100

    add retrieve and create func

--- backend_core/players/serializers.py
+++ backend_core/players/serializers.py
@@ -1,12 +1,11 @@
 from rest_framework import serializers
 
 from .models import Player
-from backend_core.technologies.serializers import TechnologiesSerializer
+from technologies.serializers import TechnologiesToPlayersSerializer
 
 
 class PlayerSerializer(serializers.ModelSerializer):
-    technologies = TechnologiesSerializer()
 
     class Meta:
         model = Player
-        fields = ('pk', 'nick', 'rank', 'is_active', 'is_online', 'technologies')
+        fields = ('pk', 'nick', 'rank', 'is_active', 'is_online',)
--- backend_core/players/viewsets.py
+++ backend_core/players/viewsets.py
@@ -1,16 +1,20 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from rest_framework import status
 from . import models
 from . import serializers
+
 
 class PlayerViewSet(ViewSet):
 
     queryset = models.Player.objects.all()
 
-
     def create(self, request):
-        pass
+        serializer = serializers.PlayerSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
         player = get_object_or_404(queryset=self.queryset, pk=pk)
