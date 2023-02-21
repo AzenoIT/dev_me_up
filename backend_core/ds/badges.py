df56106c97b0297685c2a8116211032a72759465
maciejrum <maciej.rumian2@gmail.com>
2023-02-21 20:35:39 +0100

    -m

--- backend_core/ds/badges.py
+++ backend_core/ds/badges.py
@@ -1,35 +1,31 @@
-from dev_me_up.backend_core.players.models import Player, Badge, PlayerBadge
+from dev_me_up.backend_core.players.models import Player, Badge, Player_to_Badge, Game
+from django.db.models import Q
 
 
 def generate_initiation_badge(player_id):
-    badge_id = 0
-    player = Player.objects.get(id=player_id)
-    player_badge = PlayerBadge.objects.filter(player=player, badge__id=badge_id)
+    badge_initial_id = 1
+    player_initial_badge = Player_to_Badge.objects.filter(id=player_id, badge_id=badge_initial_id).count()
+    # player_badges = PlayerBadge.objects.filter(id=player_id)
 
-    if player_badge is None:
-        badge = Badge.objects.get(id=badge_id)
-        player_badge = PlayerBadge(player=player, badge=badge)
-        player_badge.save()
+    if player_initial_badge == 0:
+        new_player_to_badge = Player_to_Badge(player_id=player_id, badge_id=badge_initial_id)
+        new_player_to_badge.save()
     else:
         pass
 
 
-def programming_intern(player_id):
-    badge_id = 1
+def programming_intern_badge(player_id):
+    badge_initial_id = 2
 
     player = Player.objects.get(id=player_id)
-    badge = Badge.objects.get(id=badge_id)
-
-    has_badge = PlayerBadge.objects.filter(player=player, badge=badge).exists()
-
-    #rozgrywka
-    won_five_games = Game.objects.filter(player=player, difficulty=1, result='W').count() >= 5
+    has_badge = Player_to_Badge.objects.filter(player=player, badge_id=badge_initial_id).exists()
+    won_five_games = Game.objects.filter(Q(ga_player1=player) | Q(ga_player2=player), ga_result=1).count() >= 5
 
     if has_badge and won_five_games:
         pass
 
     if not has_badge and won_five_games:
-        player_badge = PlayerBadge(player=player, badge=badge)
+        player_badge = Player_to_Badge(player_id=player_id, badge_id=badge_initial_id)
         player_badge.save()
 
     else:
