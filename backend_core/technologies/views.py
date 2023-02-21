7b362957dd44b2f3e046f0a03da1a7d68fa77b4c
BlazejBielski <Blazej Bielski>
2023-02-21 15:15:49 +0100

    fixed generic class

--- backend_core/technologies/views.py
+++ backend_core/technologies/views.py
@@ -1,10 +1,9 @@
 from rest_framework.generics import ListAPIView
-from rest_framework.views import APIView
 
 from technologies.models import Technology
 from technologies.serializers import TechnologiesSerializer
 
 
-class TechnologyListView(APIView):
+class TechnologyListView(ListAPIView):
     serializer_class = TechnologiesSerializer
     queryset = Technology.objects.all()
