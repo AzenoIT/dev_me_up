09f9cb764a19aab994e4a51669bab17387d38587
BlazejBielski <Blazej Bielski>
2023-02-21 19:52:07 +0100

    add url and basic userfriend create

--- backend_core/users/views.py
+++ backend_core/users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.generics import UpdateAPIView, ListAPIView, CreateAPIView, RetrieveAPIView
+from rest_framework.mixins import CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import UpdateAPIView
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
@@ -48,12 +49,19 @@ class GetUserAPIView(RetrieveAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = serializers.CustomUserSerializer
 
-
-class GetUserAPIView(RetrieveAPIView):
-    permission_classes = [IsAuthenticated]
-    queryset = get_user_model().objects.all()
-    serializer_class = serializers.CustomUserSerializer
-
     def retrieve(self, request, *args, **kwargs):
         serializer = self.serializer_class(request.user)
         return Response(serializer.data)
+
+
+class UserFriendAddAPIView(CreateAPIView):
+    queryset = models.UserFriend.objects.all()
+    serializer_class = serializers.UserFriendListSerializer
+    permission_class = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
