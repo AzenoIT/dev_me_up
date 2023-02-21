3afc16530573adcf3f71d6d0f1d6a9032f00fdc6
BlazejBielski <Blazej Bielski>
2023-02-21 21:47:28 +0100

    add views

--- backend_core/users/views.py
+++ backend_core/users/views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import UpdateAPIView
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.response import Response
+from rest_framework.validators import UniqueTogetherValidator
 
 from . import serializers
 from . import models
