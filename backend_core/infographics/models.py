017dd88e8824ed80ccb2d2b5be527c275cbbba25
Aleksander Kluska <kluskaolek2002@gmail.com>
2023-02-21 13:18:03 +0100

    rename value field to information

--- backend_core/infographics/models.py
+++ backend_core/infographics/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Infographic(models.Model):
     name = models.CharField(max_length=50)
-    value = models.CharField(max_length=255)
+    information = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
