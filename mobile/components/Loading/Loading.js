5e169d4cc3719ea59f60d53b075dc7f96d5fce98
Andrzej Legutko <andrzejlegutko@gmail.com>
2023-02-21 10:48:02 +0100

    Push notification testing

--- mobile/components/Loading/Loading.js
+++ mobile/components/Loading/Loading.js
@@ -1,22 +1,107 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform } from 'react-native';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 
-function Loading() {
+Notifications.setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: true,
+		shouldSetBadge: false,
+	}),
+});
+
+export default function App() {
+	const [expoPushToken, setExpoPushToken] = useState('');
+	const [notification, setNotification] = useState(false);
+	const notificationListener = useRef();
+	const responseListener = useRef();
+
+	useEffect(() => {
+		registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+		notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+			setNotification(notification);
+		});
+
+		responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+			console.log(response);
+
+			const sendNotification = async () => {
+				await schedulePushNotification();
+			}
+
+			sendNotification();
+		});
+
+		return () => {
+			Notifications.removeNotificationSubscription(notificationListener.current);
+			Notifications.removeNotificationSubscription(responseListener.current);
+		};
+	}, []);
+
 	return (
-		<View style={styles.container}>
-			<Text>Loading</Text>
-			<StatusBar style="auto" />
+		<View
+			style={{
+				flex: 1,
+				alignItems: 'center',
+				justifyContent: 'space-around',
+			}}>
+			<Text>Your expo push token: {expoPushToken}</Text>
+			<View style={{ alignItems: 'center', justifyContent: 'center' }}>
+				<Text>Title: {notification && notification.request.content.title} </Text>
+				<Text>Body: {notification && notification.request.content.body}</Text>
+				<Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+			</View>
+			<Button
+				title="Press to schedule a notification"
+				onPress={async () => {
+					await schedulePushNotification();
+				}}
+			/>
 		</View>
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
+async function schedulePushNotification() {
+	await Notifications.scheduleNotificationAsync({
+		content: {
+			title: "DevMeUp",
+			body: 'Here is the notification body',
+			data: { data: 'goes here' },
+		},
+		trigger: { seconds: 2 },
+	});
+}
 
-export default Loading;
+async function registerForPushNotificationsAsync() {
+	let token;
+
+	if (Platform.OS === 'android') {
+		await Notifications.setNotificationChannelAsync('default', {
+			name: 'default',
+			importance: Notifications.AndroidImportance.MAX,
+			vibrationPattern: [0, 250, 250, 250],
+			lightColor: '#FF231F7C',
+		});
+	}
+
+	if (Device.isDevice) {
+		const { status: existingStatus } = await Notifications.getPermissionsAsync();
+		let finalStatus = existingStatus;
+		if (existingStatus !== 'granted') {
+			const { status } = await Notifications.requestPermissionsAsync();
+			finalStatus = status;
+		}
+		if (finalStatus !== 'granted') {
+			alert('Failed to get push token for push notification!');
+			return;
+		}
+		token = (await Notifications.getExpoPushTokenAsync()).data;
+		console.log(token);
+	} else {
+		alert('Must use physical device for Push Notifications');
+	}
+
+	return token;
+}
---
+++ mobile/helpers/registerForPushNotification.js
@@ -0,0 +1,36 @@
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+
+async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === 'android') {
+        await Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+        });
+    }
+
+    if (Device.isDevice) {
+        const {status: existingStatus} = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const {status} = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+    } else {
+        alert('Must use physical device for Push Notifications');
+    }
+
+    return token;
+}
+
+export default registerForPushNotificationsAsync;
