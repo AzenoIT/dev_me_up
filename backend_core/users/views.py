1af2ba9088b8af110370de5b015e51a0a98528be
t.kajdrowicz <t.kajdrowicz@aotm.gov.pl>
2023-02-21 18:50:07 +0100

    user's friends list - second attempt

--- backend_core/users/views.py
+++ backend_core/users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import UpdateAPIView, ListAPIView, CreateAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from . import serializers
@@ -25,3 +26,26 @@ class UserFriendListAPIView(ListAPIView):
         user_id = request.user.id
         serializer = serializers.UserFriendListSerializer(self.queryset.filter(user__pk=user_id), many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = serializers.CustomUserSerializer
+    queryset = get_user_model().objects.all()
+    permission_class = [IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        serializer = self.serializer_class(request.user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class GetUserAPIView(RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = get_user_model().objects.all()
+    serializer_class = serializers.CustomUserSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        serializer = self.serializer_class(request.user)
+        return Response(serializer.data)
