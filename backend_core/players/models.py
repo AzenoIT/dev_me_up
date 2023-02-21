72992eb3ab7891fa59e5355448c5459ae7525ab0
Martyna Witkowska <mar.witkowska@gmail.com>
2023-02-21 09:49:46 +0100

    add is active and is online fields

--- backend_core/players/models.py
+++ backend_core/players/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Player(models.Model):
     nick = models.CharField(max_length=30)
     rank = models.FloatField()
+    is_active = models.FloatField(default=False)
+    is_online = models.FloatField(default=False)
     is_bot = models.BooleanField(default=False)
 
     def __str__(self):
