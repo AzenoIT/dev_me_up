66bede8ec56d2232df22a0935a3918d9030868b5
t.kajdrowicz <t.kajdrowicz@aotm.gov.pl>
2023-02-21 10:59:24 +0100

    __str__ update

--- backend_core/applications/models.py
+++ backend_core/applications/models.py
@@ -14,4 +14,4 @@ class Application(models.Model):
     accounts = models.ManyToManyField('users.CustomUser', related_name='%(class)s')
 
     def __str__(self):
-        return self.name
+        return self.device_identity
