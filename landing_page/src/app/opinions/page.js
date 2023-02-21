e98be4e939aae7f9d3eb86539b24b146c349f06e
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2023-02-21 11:50:32 +0100

    Fix problem with font

--- landing_page/src/app/opinions/page.js
+++ landing_page/src/app/opinions/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import '../../styles/opinions.css'
-import {roboto300, roboto500, roboto700} from "@/app/fonts";
+import {roboto300, roboto500} from "@/app/fonts";
 import Image from "next/image";
 import google from '../../images/Badgegoogle.png'
 import apple from '../../images/Badgeapple.png'
@@ -9,8 +9,8 @@ import apple from '../../images/Badgeapple.png'
 function Opinions() {
     return (
         <div className='container'>
-            <h2 className={`${roboto500} hdl`}>Zabawa i&nbsp;nauka w jednym</h2>
-            <p className={`${roboto300} text`}>Sprawdź swoją wiedzę rywalizując&nbsp;ze znajomymi</p>
+            <h2 className={`${roboto500.className} hdl`}>Zabawa i&nbsp;nauka w jednym</h2>
+            <p className={`${roboto300.className} text`}>Sprawdź swoją wiedzę rywalizując&nbsp;ze znajomymi</p>
             <a href='https://www.apple.com/pl/app-store/' className='btnAppStore' variant="outlined"><Image src={apple} alt='Apple picture'/></a>
             <a href='https://play.google.com/store/games?hl=pl&gl=US' className='btnGooglePlay' variant="outlined"><Image src={google} alt='Google picture'/></a>
         </div>
