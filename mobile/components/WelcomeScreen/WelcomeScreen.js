e84eda2621a7d61fa34e9bfa43b26d2c3f4a5fdb
Pawel Konior <pawel.konior@gmail.com>
2023-02-21 16:16:11 +0100

    api call helper and check communication

--- mobile/components/WelcomeScreen/WelcomeScreen.js
+++ mobile/components/WelcomeScreen/WelcomeScreen.js
@@ -1,33 +1,61 @@
+import React, {useEffect, useState} from "react";
 import SignUp from "../SignUp/SignUp";
 import Login from "../Login/Login";
 import {Button, useTheme} from "react-native-paper";
 import {TextInput, View} from "react-native";
 import {link} from "../../helpers/helpers";
 import {StyleSheet} from "react-native";
-import {useState} from "react";
+import callApi from "../../helpers/api";
+import { Text } from 'react-native-paper';
 
 
 function WelcomeScreen({navigation}) {
     const [guestName, setGuestName] = useState("");
     const theme = useTheme();
 
+    useEffect(() => {
+        const controller = new AbortController();
+        callApi({
+            endpoint: '/userNames',
+            signal: controller.signal
+        })
+            .then((data) => {
+                setGuestName(data[0].name)
+                alert(data[0].name);
+            })
+            .catch((err) => {
+                alert(err);
+            });
+
+        return () => {
+            controller.abort();
+        }
+    }, []);
+
     const styles = StyleSheet.create({
         btn: {
             margin: 5,
+            maxWidth: '80%',
+            width: "100%",
         },
         play_btn: {
             margin: 5,
-            backgroundColor: theme.colors.onBackground
+            backgroundColor: theme.colors.onBackground,
+            maxWidth: '80%',
+            width: "100%",
         },
         input: {
             borderWidth: 1,
             borderStyle: "solid",
             borderColor: theme.colors.primary,
-            margin: 20
+            margin: 20,
+            width: "100%",
+            maxWidth: '80%'
         },
         container: {
             flex: 1,
             justifyContent: "center",
+            alignItems: "center",
         }
     })
 
@@ -41,6 +69,7 @@ function WelcomeScreen({navigation}) {
                 type={"outlined"}
                 style={styles.input}
             />
+            <Text variant="displayLarge" style={{color: 'red'}}>{guestName}</Text>
 
             <Button
                 icon="account-eye-outline"
