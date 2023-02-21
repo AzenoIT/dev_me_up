d5c41fb863a6517a5fe13c2eae6afcabcb9912ce
t.kajdrowicz <t.kajdrowicz@aotm.gov.pl>
2023-02-21 10:45:51 +0100

    application, device_information models

--- backend_core/applications/admin.py
+++ backend_core/applications/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+# TODO - this filtering does not make many sense, does it?
+class DeviceInformationAdmin(admin.ModelAdmin):
+    fields = ('device_dictionary',)
+    list_display = ('pk', 'device_dictionary')
+    search_fields = ('device_dictionary',)
+    list_filter = ('device_dictionary',)
+    ordering = ('-device_dictionary',)
+
+
+class ApplicationAdmin(admin.ModelAdmin):
+    fields = ('device_identity', 'creation_date')
+    list_display = ('pk', 'device_identity', 'creation_date')
+    search_fields = ('device_identity',)
+    list_filter = ('device_identity', 'creation_date')
+    ordering = ('-creation_date',)
+
+
+admin.site.register(models.DeviceInformation)
+admin.site.register(models.Application, ApplicationAdmin)
--- backend_core/applications/models.py
+++ backend_core/applications/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
+
 class DeviceInformation(models.Model):
-    device_dictionary = models.CharField(max_length=100)
+    device_information = models.CharField(max_length=100)
 
 
-class Applications(models.Model):
-    device_dictionary = models.ForeignKey('DeviceDictionary', on_delete=models.DO_NOTHING, related_name='%(class)s')
+class Application(models.Model):
+    device_information = models.ForeignKey('DeviceInformation', on_delete=models.DO_NOTHING, related_name='%(class)s')
 
     device_identity = models.CharField(max_length=50, unique=True)
-    creation_date = models.TextField()
-    technologies = models.ManyToManyField('technologies.Technologies', related_name='applications')
-    players = models.ManyToManyField('players.Players', related_name='applications')
+    creation_date = models.DateTimeField()
+    players = models.ManyToManyField('players.Player', related_name='%(class)s')
+    accounts = models.ManyToManyField('users.CustomUser', related_name='%(class)s')
 
     def __str__(self):
         return self.name
