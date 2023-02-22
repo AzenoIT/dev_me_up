73719243777e4a411787820d87fc8a8632d6c9cc
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2023-02-22 10:46:18 +0100

    Add qr code an downloads buttons to main page, add new fonts, fixed problem with buttons in first and end section

--- landing_page/src/app/end/page.js
+++ landing_page/src/app/end/page.js
@@ -9,9 +9,10 @@ function EndSection(props) {
             <h2 className={`${roboto500.className} hdl_end`}>Zabawa i&nbsp;nauka w jednym</h2>
             <p className={`${roboto300.className} text_end`}>Sprawdź swoją wiedzę rywalizując ze znajomymi.</p>
 
-            <a href="" className={`${roboto500.className} btn_newsletter_add btn_download`}>Pobierz aplikację</a>
-            <a href="" className={`${roboto500.className} btn_newsletter_add`}>Newsletter</a>
-
+            <div className='buttons_end'>
+                <a href="" className={`${roboto500.className} btn_newsletter_add btn_download`}>Pobierz aplikację</a>
+                <a href="" className={`${roboto500.className} btn_newsletter_add`}>Newsletter</a>
+            </div>
         </div>
     );
 }
