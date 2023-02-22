add5815c0adbd03e17919cd3ac77663f0b83d459
BlazejBielski <Blazej Bielski>
2023-02-22 13:06:19 +0100

    delete unused imports

--- backend_core/users/views.py
+++ backend_core/users/views.py
@@ -1,12 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.generics import UpdateAPIView, ListAPIView, CreateAPIView, RetrieveAPIView
-from rest_framework.mixins import CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.generics import UpdateAPIView
-from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.response import Response
-from rest_framework.validators import UniqueTogetherValidator
 
 from . import serializers
 from . import models
