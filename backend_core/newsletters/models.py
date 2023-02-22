9becbbdfff95bbcd97ba2f6429e5b631f349cef1
Rafał Płoszański <rafalploszanski@interia.pl>
2023-02-22 19:27:23 +0100

    add fields to models and admin

---
+++ backend_core/newsletters/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Subscriber(models.Model):
+    email = models.EmailField(unique=True)
+    confirmed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email + " (" + str(self.confirmed) + ")"
+
+
+class Newsletter(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    subject = models.CharField(max_length=150)
+    contents = models.FileField(upload_to='uploaded_newsletters/')
+
+    def __str__(self):
+        return self.subject + " " + self.created_at.strftime("%B %d, %Y")
