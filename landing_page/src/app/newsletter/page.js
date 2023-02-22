0028ec770124398f6d10c053e1fd26217e2538d2
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2023-02-22 16:39:08 +0100

    Naming fixes

--- landing_page/src/app/newsletter/page.js
+++ landing_page/src/app/newsletter/page.js
@@ -10,7 +10,7 @@ function Newsletter() {
         <div id="newsletter" className="box_newsletters hide-on-desktop">
             <h2 className={`${roboto500.className} hdl_newsletter`}>Graj Rywalizuj Ucz się</h2>
             <p className={`${roboto300.className} text_newsletter`}>Sprawdź swoją wiedzę rywalizując ze znajomymi.</p>
-            <div className='dupa'>
+            <div className='btn_newsletter_box'>
             <a href="" className={`${roboto500.className} btn_newsletter_add`}>Newsletter</a>
             </div>
         </div>
