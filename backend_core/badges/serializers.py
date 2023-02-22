1102c035aaf5c70bac9b0c934b74421c843cbfe7
Bartosz Baran <bartosz.baran192@gmail.com>
2023-02-22 17:52:49 +0100

    fix model serializer badges

--- backend_core/badges/serializers.py
+++ backend_core/badges/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 
 from . import models
 
-class BadgeSerializer(serializers.Modelserializer):
+class BadgeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Badge
         fields = ('name', 'weight', 'points')
 
 
-class PlayersToBadgeSerializer(serializers.Modelserializer):
+class PlayersToBadgeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.PlayersToBadge
         fields = ('player', 'badge', 'creation_date')
