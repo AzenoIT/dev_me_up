a77f968eb19ef0ac162d2c1cd5459d634166677b
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2023-02-23 11:59:35 +0100

    Add new images

--- landing_page/src/app/newsletter/page.js
+++ landing_page/src/app/newsletter/page.js
@@ -2,7 +2,7 @@
 
 import "../../styles/scss/components/newsletter.scss";
 import "../../styles/scss/helpers/buttons.scss";
-import {roboto300, roboto400, roboto500, teko400} from "@/app/fonts";
+import {roboto300, roboto400, roboto500, teko300, teko400, teko500} from "@/app/fonts";
 
 
 
@@ -10,8 +10,8 @@ function Newsletter() {
     return (
         <>
             <div id="newsletter" className="hide-on-desktop  box_newsletters">
-                <h2 className={`${roboto500.className} hdl_newsletter`}>Graj Rywalizuj Ucz się</h2>
-                <p className={`${roboto300.className} text_newsletter`}>Sprawdź swoją wiedzę rywalizując ze
+                <h2 className={`${teko500.className} hdl_newsletter`}>Graj Rywalizuj Ucz się</h2>
+                <p className={`${teko300.className} text_newsletter`}>Sprawdź swoją wiedzę rywalizując ze
                     znajomymi.</p>
                 <div className='btn_newsletter_box'>
                     <a href="" className={`${roboto500.className} btn_newsletter_add`}>Newsletter</a>
