8c87606d615586809191e05251034f51d959e1e6
Aleksander Kluska <kluskaolek2002@gmail.com>
2023-02-23 12:52:50 +0100

    add serializer for player friend model

--- backend_core/players/serializers.py
+++ backend_core/players/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Player
+from .models import Player, PlayerFriend
 from technologies.serializers import TechnologiesToPlayersSerializer
 from badges.serializers import PlayersToBadgeSerializer
 
@@ -21,3 +21,9 @@ class PlayerDetailSerializer(serializers.ModelSerializer):
         model = Player
         fields = ('pk', 'uuid', 'nick', 'rank', 'is_active', 'is_online', 'is_bot', 'is_search_visible',
                   'is_rank_visible', 'theme', 'technologiestoplayers', 'playerstobadge')
+
+
+class PlayerFriendSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlayerFriend
+        fields = ('player', 'friend', 'approved', 'relation')
