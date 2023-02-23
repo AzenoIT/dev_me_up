ae199c7116e395a62378c878bc4bd43f12057bde
olivergav <oliwier.gawlik0@gmail.com>
2023-02-23 13:41:05 +0100

    changes in index

--- pwa/src/index.js
+++ pwa/src/index.js
@@ -3,8 +3,8 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom/client';
 import {
-    createBrowserRouter,
-    RouterProvider,
+	createBrowserRouter,
+	RouterProvider,
 } from "react-router-dom";
 import Category from "./components/Category/Category";
 import Tutorial from "./components/Tutorial/Tutorial";
@@ -16,65 +16,68 @@ import Profile from "./components/Profile/Profile";
 import {AuthProvider} from "./context/AuthProvider";
 import Start from "./components/Start/Start";
 import Start2 from "./components/Start/Start2";
-import StartFirst from "./components/Start/StartFirst";
-import Battle from "./components/Battle/Battle";
+import Friends from "./components/Rankings/Friends";
+import StartFirst from "./components/Start/StartFirst"
+import BattleSummary from "./components/Battle/BattleSummary";
 
 const router = createBrowserRouter([
 	{
-		path: "/start",
-		element: <Start />,
-	},
-	{
-		path: "/start2",
-		element: <Start2 />,
-	},
-	{
-		path: "/startfirst",
-		element: <StartFirst />,
-	},
-	{
-		path: "/battle",
-		element: <Battle />,
+		path: '/start2',
+		element: <Start2/>
 	},
 	{
 		path: "/tutorial",
-		element: <Tutorial />,
+		element: <Tutorial/>
 	},
 	{
-		path: "/",
-		element: <Layout />,
+		path: '/',
+		element: <Layout/>,
 		children: [
 			{
-				path: "intro",
-				element: <Intro />,
+				path: 'start',
+				element: (<Start/>)
+
+			},
+			{
+				path: 'startfirst',
+				element: (<StartFirst/>)
+
+			},
+			{
+				path: "/profile",
+				element: <Profile/>
 			},
 			{
 				path: "category",
-				element: <Category />,
+				element: <Category/>
 			},
 			{
-				path: "profile",
-				element: <Profile />,
+				path: 'login',
+				element: (<Login/>)
 			},
 			{
-				path: "login",
-				element: <Login />,
+				path: 'register',
+				element: (<Register/>)
 			},
 			{
-				path: "register",
-				element: <Register />,
+				path: 'friends',
+				element: <Friends/>
 			},
-		],
-	},
-]);
+			{
+				path: 'summary',
+				element: <BattleSummary/>
+			}
+		]
+	}
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <AuthProvider>
-            <RouterProvider router={router}/>
-        </AuthProvider>
-    </React.StrictMode>
+	<React.StrictMode>
+		<AuthProvider>
+			<RouterProvider router={router}/>
+		</AuthProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
