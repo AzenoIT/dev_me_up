b3eab602c8cd35506a9350da6444a711adf9e665
olivergav <oliwier.gawlik0@gmail.com>
2023-03-02 17:35:20 +0100

    Add redirecting to StartFirst, if user is new

--- pwa/src/components/Layout/Layout.jsx
+++ pwa/src/components/Layout/Layout.jsx
@@ -1,21 +1,21 @@
-import {Outlet} from 'react-router-dom';
+import {useEffect, useState} from "react";
+import {Outlet, useNavigate} from 'react-router-dom';
 import {ThemeProvider, createTheme} from '@mui/material/styles';
-
-import NavBar from "../NavBar/NavBar";
 import Box from "@mui/material/Box";
-import {NavigateProvider} from "../../context/NavigateProvider";
-
-
-const lightTheme = createTheme({
-    palette: {
-        mode: 'light',
-        primary: {
-            main: '#ffffff',
-        },
-    },
-});
+import NavigateContext, {NavigateProvider} from "../../context/NavigateProvider";
+import {useLocalStorage} from "../../hooks/useLocalStorage";
+import NavBar from "../NavBar/NavBar";
 
 function Layout() {
+    const navigate = useNavigate();
+    const profileState = useState(useLocalStorage('profile', '')[0] || '')[0];
+
+    useEffect(() => {
+        if (!profileState) {
+            navigate('/startfirst');
+        }
+    }, [])
+
     return (
         <>
             <NavigateProvider>
@@ -26,9 +26,17 @@ function Layout() {
                     </Box>
                 </ThemeProvider>
             </NavigateProvider>
-
         </>
     )
 }
+
+const lightTheme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#ffffff',
+        },
+    },
+});
 
 export default Layout;
